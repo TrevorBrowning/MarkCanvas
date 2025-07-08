b42389f13a0a1186c144ca38b5319f4a3f064bd2
TrevorBrowning <trevryanbrowning@gmail.com>
2025-07-08 04:22:52 -0400

    fix: vite.config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/markcanvas/',
+  base: '/MarkCanvas/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
